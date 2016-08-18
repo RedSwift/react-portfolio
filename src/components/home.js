2e131acd1f31304d34da7796194726dccf8d6696
Dominic <jxdlam@gmail.com>
2016-08-18 16:11:17 +0800

    finetuned interface for home page

--- src/components/home.js
+++ src/components/home.js
@@ -1,12 +1,22 @@
 import React, { Component } from 'react'
 import AppBar from './app_bar'
+import { RaisedButton } from 'material-ui'
 
 class Home extends Component {
   render () {
     return (
       <div>
         <AppBar />
-        <h2 className='my-name' >Dominic Lam</h2>
+        <div className='main-cont'>
+          <h2 className='my-name' >Dominic Lam</h2>
+          <img className='front-pic' src={'http://oi64.tinypic.com/1ta3jn.jpg'} />
+          <h3>Singapore Based Web Developer</h3>
+          <div className='button-group flex'>
+            <RaisedButton className='margin-right' primary={true} label='Github'/>
+            <RaisedButton className='margin-right' primary={true} label='LinkedIn'/>
+            <RaisedButton primary={true} label='GA Profile'/>
+          </div>
+        </div>
       </div>
     )
   }
